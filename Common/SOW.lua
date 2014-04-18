d137ff4818899d8aa0378179de6059f26d0f3405
honda7 <hond477@gmail.com>
2014-04-18 17:31:48 +0200

    Update SOW.lua

--- Common/SOW.lua
+++ Common/SOW.lua
@@ -1,30 +1,29 @@
 local version = "1.127"
 local AUTOUPDATE = true
-local UPDATE_HOST = "bitbucket.org"
-local UPDATE_PATH = "/honda7/bol/raw/master/Common/SOW.lua".."?rand="..math.random(1,10000)
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/honda7/BoL/master/Common/SOW.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = LIB_PATH.."SOW.lua"
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
-function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>SOW:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+function _AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>SOW:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
+	local ServerData = GetWebResult(UPDATE_HOST, "/honda7/BoL/master/VersionFiles/SOW.version")
 	if ServerData then
-		local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
-		ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
-			ServerVersion = tonumber(ServerVersion)
+			print(ServerVersion)
 			if tonumber(version) < ServerVersion then
-				AutoupdaterMsg("New version available"..ServerVersion)
-				AutoupdaterMsg("Updating, please don't press F9")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)			else
-				--AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+				_AutoupdaterMsg("New version available"..ServerVersion)
+				_AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () _AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+			else
+				_AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
 			end
 		end
 	else
-		--AutoupdaterMsg("Error downloading version info")
+		_AutoupdaterMsg("Error downloading version info")
 	end
 end
-
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
